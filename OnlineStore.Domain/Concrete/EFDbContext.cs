a0f2fceeb904885f9dddb090fa0b4ef1fbfd21b9
Evgeniy Bocharov <bocharovevgen@gmail.com>
2015-07-14 04:06:09 +0300

    new origin commit

---
+++ OnlineStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,10 @@
+﻿using OnlineStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace OnlineStore.Domain.Concrete
+{
+    class EFDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ OnlineStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.WebUI.Binders
+{
+    public class CartModelBinder: IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // get the Cart from session
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+
+            // create the Cart if there wasn`t one in the session data
+            if (cart == null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+
+            //return the cart
+            return cart;
+
+        }
+    }
+}
